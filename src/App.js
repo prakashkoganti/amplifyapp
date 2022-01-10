525541568c11e504360dee91f6f26cd9b9a7d95c
prakashkoganti <koganti.prakash@gmail.com>
2022-01-09 21:08:56 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Testing
+          Learn React First
         </a>
       </header>
     </div>
